0e2b6940a88ac0bac2724c8728cbaabed5930b72
Eden <edennissinman@gmail.com>
2023-05-16 13:19:47 +0300

    16_05

--- pretrained_models.py
+++ pretrained_models.py
@@ -1,8 +1,9 @@
 from torchvision.models import densenet121, DenseNet121_Weights, googlenet, GoogLeNet_Weights, vgg16, VGG16_Weights, \
     wide_resnet101_2, Wide_ResNet101_2_Weights
 from efficientnet_pytorch import EfficientNet
-
+import torch
 from torch import nn
+import torch.nn.functional as F
 
 
 def load_pretrained_model(model_name, num_classes):
@@ -18,34 +19,138 @@ def load_pretrained_model(model_name, num_classes):
         for param in model.parameters():
             param.requires_grad = False
         num_features = model.classifier.in_features
-        model.classifier = nn.Linear(num_features, num_classes)
+        model.classifier = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
     elif model_name == 'EfficientNet':
         model = EfficientNet.from_pretrained('efficientnet-b0')
         for param in model.parameters():
             param.requires_grad = False
         num_features = model._fc.in_features
-        model._fc = nn.Linear(num_features, num_classes)
+        model._fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
     elif model_name == 'GoogLeNet':
         WEIGHTS = GoogLeNet_Weights.DEFAULT
         model = googlenet(weights=WEIGHTS)
         for param in model.parameters():
             param.requires_grad = False
         num_features = model.fc.in_features
-        model.fc = nn.Linear(num_features, num_classes)
+        model.fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
     elif model_name == 'VGG':
         WEIGHTS = VGG16_Weights.DEFAULT
         model = vgg16(weights=WEIGHTS)
         for param in model.parameters():
             param.requires_grad = False
         num_features = model.classifier[6].in_features
-        model.fc = nn.Linear(num_features, num_classes)
+        model.fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
     elif model_name == 'Wide_ResNet':
         WEIGHTS = Wide_ResNet101_2_Weights.DEFAULT
         model = wide_resnet101_2(weights=WEIGHTS)
         for param in model.parameters():
             param.requires_grad = False
         num_features = model.fc.in_features
-        model.fc = nn.Linear(num_features, num_classes)
+        model.fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
+    else:
+        raise ValueError("The pre-trained model must be either of the following:"
+                         " DenseNet; EfficientNet; GoogLeNet; VGG; Wide_ResNet")
+    return model
+
+
+def pretrained_model_whole(model_name, num_classes):
+    """
+
+    :param model_name: can be either of the following:
+    DenseNet; EfficientNet; GoogLeNet; VGG; Wide_ResNet
+    :return: the trained model
+    """
+    if model_name == 'DenseNet':
+        WEIGHTS = DenseNet121_Weights.DEFAULT
+        model = densenet121(weights=WEIGHTS)
+        num_features = model.classifier.in_features
+        model.classifier = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
+    elif model_name == 'EfficientNet':
+        model = EfficientNet.from_pretrained('efficientnet-b0')
+        num_features = model._fc.in_features
+        model._fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
+    elif model_name == 'GoogLeNet':
+        WEIGHTS = GoogLeNet_Weights.DEFAULT
+        model = googlenet(weights=WEIGHTS)
+        num_features = model.fc.in_features
+        model.fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
+    elif model_name == 'VGG':
+        WEIGHTS = VGG16_Weights.DEFAULT
+        model = vgg16(weights=WEIGHTS)
+        num_features = model.classifier[6].in_features
+        model.fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
+    elif model_name == 'Wide_ResNet':
+        WEIGHTS = Wide_ResNet101_2_Weights.DEFAULT
+        model = wide_resnet101_2(weights=WEIGHTS)
+        num_features = model.fc.in_features
+        model.fc = nn.Sequential(
+            nn.Linear(num_features, num_classes),
+            nn.Softmax()
+        )
+    else:
+        raise ValueError("The pre-trained model must be either of the following:"
+                         " DenseNet; EfficientNet; GoogLeNet; VGG; Wide_ResNet")
+    return model
+
+
+def load_model4test(model_name, num_classes, weights_path):
+    """
+
+    :param model_name: can be either of the following:
+    DenseNet; EfficientNet; GoogLeNet; VGG; Wide_ResNet
+    :return: the trained model
+    """
+    if model_name == 'DenseNet':
+        model = densenet121(pretrained=False)
+        model.classifier = nn.Linear(1024, num_classes)
+        model.load_state_dict(torch.load(weights_path))
+
+    elif model_name == 'EfficientNet':
+        model = EfficientNet.from_name('efficientnet-b0', num_classes=num_classes)
+        model.load_state_dict(torch.load(weights_path))
+
+    elif model_name == 'GoogLeNet':
+        model = googlenet(pretrained=False)
+        model.fc = nn.Linear(1024, num_classes)
+        model.load_state_dict(torch.load(weights_path))
+
+    elif model_name == 'VGG':
+        model = vgg16(pretrained=False)
+        model.classifier[6] = nn.Linear(4096, num_classes)
+        model.load_state_dict(torch.load(weights_path))
+
+    elif model_name == 'Wide_ResNet':
+        model = wide_resnet101_2(pretrained=False)
+        model.fc = nn.Linear(2048, num_classes)
+        model.load_state_dict(torch.load(weights_path))
+
     else:
         raise ValueError("The pre-trained model must be either of the following:"
                          " DenseNet; EfficientNet; GoogLeNet; VGG; Wide_ResNet")
--- test.py
+++ test.py
@@ -1,45 +1,68 @@
 import torch
-from data_loading import data_loaders
+from dataloader import data_loaders
 from Gesture_class import CNN
 import argparse
 import warnings
 import torch.nn as nn
+from pretrained_models import load_model4test
+from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser(description='Test Config', add_help=False)
 
-parser.add_argument('--root_test', default=r'', metavar='DIR',
+parser.add_argument('--root_test', default=r'/home/roblab20/PycharmProjects/LongRange/data/data_LongRANGE', metavar='DIR',
                     help='path to training dataset')
-parser.add_argument('--root_checkpoint', default=r'', metavar='DIR',
-                    help='path to training dataset')
+parser.add_argument('--csv_root', default=r'data/LongeRange_CSV/data_all.csv', metavar='DIR',
+                    help='path to csv dataset')
+parser.add_argument('--img_root', default=r'/home/roblab20/PycharmProjects/LongRange/data/data_LongRANGE', metavar='DIR',
+                    help='path to csv dataset')
+parser.add_argument('--root_checkpoint', default=r'checkpoint/DenseNet/05_11_2023/24_net_Thu_May_11_16_50_20_2023.pt',
+                    metavar='DIR', help='path to training dataset')
 parser.add_argument('--img_size', type=int, default=224, metavar='Size',
                     help='Image size for resize')
+parser.add_argument('--batch_size', type=int, default=1, metavar='N',
+                    help='input batch size for training (default: 16)')
+parser.add_argument("--drop_last", default=True, type=str)
+parser.add_argument('--num_classes', type=int, default=5,
+                    help='Number of classes to classify')
+parser.add_argument('--pretrained', default=True, type=bool,
+                    help='Use pretrained model. (default: false)')
+parser.add_argument('--pretrained_model', default='DenseNet', type=str,
+                    help='Pretrained model can be either: DenseNet; EfficientNet; GoogLeNet; VGG; Wide_ResNet')
+parser.add_argument('-j', '--workers', type=int, default=1, metavar='N',
+                    help='how many training processes to use (default: 2)')
 
 
 def evaluate_model(test_set, model, device):
     gt_label = []
     preds = []
-    for img, label in test_set:
+    criterion = nn.CrossEntropyLoss()
+
+    pbar = tqdm(test_set, total=len(test_set))
+    for img, label in pbar:
         test_x = img.to(device)
         label = label.to(device)
 
-        gt_label.append(label.item())
+        gt_label.append(label[0].detach().cpu())
 
         with torch.no_grad():
             output = model(test_x)
 
-        preds.append(output.item())
+        preds.append(output[0].detach().cpu())
 
-    gt_label = torch.tensor(gt_label)
-    preds = torch.tensor(preds)
+    gt_label = torch.stack(gt_label)
+    preds = torch.stack(preds)
 
-    criterion = nn.CrossEntropyLoss()
-    ce = criterion(gt_label, preds)
-
-    _, pred_class = torch.max(preds, dim=1)
-    label_class = torch.nonzero(gt_label == 1, as_tuple=False)[:, 1]
-    acc = torch.sum(torch.eq(pred_class, label_class)).item() / len(label_class)
+    ce = criterion(gt_label[0], preds[0])
+    equals = []
+    for i in range(len(gt_label)):
+        _, pred_class = torch.max(preds[i], dim=0)
+        label_class = torch.nonzero(gt_label[i] == 1, as_tuple=False)
+        eq = torch.eq(pred_class, label_class)
+        equals.append(eq.item())
+    integer_list = list(map(int, equals))
+    acc = sum(integer_list) / len(integer_list)
 
     print(f'Test CE: {ce}. Test Accuracy: {acc}')
 
@@ -50,11 +73,18 @@ if __name__ == "__main__":
 
     args_config = parser.parse_args()
 
-    _, _, test_dataloader = data_loaders(args_config, test=True)
+    classes = {'None': 0, 'Point': 1, 'Bad': 2, 'Good': 3, 'Stop': 4}
+    _, _, test_dataloader = data_loaders(args_config, classes)
 
     model_path = args_config.root_checkpoint
-    model = CNN(device)
-    model.load_state_dict(torch.load(model_path, map_location=device))
+
+    if args_config.pretrained:
+        model = load_model4test(args_config.pretrained_model, args_config.num_classes, args_config.root_checkpoint)
+        model.to(device)
+    else:
+        model = CNN(args_config.device)
+        model.load_state_dict(torch.load(model_path, map_location=device))
+
     model.eval()
 
     evaluate_model(test_dataloader, model, device)
