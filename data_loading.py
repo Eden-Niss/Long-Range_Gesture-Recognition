27d1a0cdcf0a35e3a84e94a4b3a0213449650fac
Eden <edennissinman@gmail.com>
2023-05-23 09:24:45 +0300

    23_05

--- data_loading.py
+++ data_loading.py
@@ -4,12 +4,13 @@ from PIL import Image
 import os
 import torch
 import pickle
+from sklearn.model_selection import train_test_split
 
 
 def get_filepaths(path):
     name_img = []
     for root, directories, files in os.walk(path):
-        if root.endswith('image'):
+        if root.endswith('crop'):
             for filename in files:
                 name_img.append(os.path.join(root, filename))
     return name_img
@@ -42,7 +43,7 @@ class ClassDataset(Dataset):
         image_class = self.images[index].split("/")[-1].split("_")[0]
         I_matrix = torch.eye(len(self.input_types))
         label = I_matrix[self.input_types.index(image_class)]
-        for i in range(2, 21):
+        for i in range(1, 21):
             full_path = self.images[index]
             if os.path.exists(full_path):
                 image_path = full_path
@@ -54,32 +55,13 @@ class ClassDataset(Dataset):
         return img, label
 
 
-def data_loaders(args, test=False):
+def data_loaders(args):
     data_transforms = {
             'train':
                 transforms.Compose([
                     transforms.Resize((args.img_size, args.img_size)),
-                    transforms.ToTensor(),
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                ]),
-            'jitter_aug':
-                transforms.Compose([
-                    transforms.Resize((args.img_size, args.img_size)),
-                    transforms.ColorJitter(brightness=1.0, contrast=0.1, saturation=0.5, hue=0.1),
-                    transforms.ToTensor(),
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                ]),
-            'blur':
-                transforms.Compose([
-                    transforms.Resize((args.img_size, args.img_size)),
-                    transforms.ToTensor(),
+                    transforms.ColorJitter(brightness=.5, hue=.3),
                     transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                ]),
-            'flip':
-                transforms.Compose([
-                    transforms.Resize((args.img_size, args.img_size)),
-                    transforms.RandomHorizontalFlip(p=1),
                     transforms.ToTensor(),
                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                 ]),
@@ -97,43 +79,27 @@ def data_loaders(args, test=False):
                 ])
         }
 
-    if test:
-        test_path = args.root_test
-        test_img_list = ['Point', 'None', 'Bad', 'Good', 'Stop']
+    train_path = args.root_train
 
-        test_dataset = ClassDataset(test_path, test_img_list, data_transforms['test'])
+    input_types = ['None', 'Point', 'Bad', 'Good', 'Stop', 'Come']
+    train_img_list = get_filepaths(train_path)
 
-        testloader = torch.utils.data.DataLoader(test_dataset, batch_size=1,
-                                                 shuffle=False, num_workers=0, drop_last=False)
-        return testloader
+    train_data, test_data = train_test_split(train_img_list, test_size=0.1, random_state=42)
+    train_data, val_data = train_test_split(train_data, test_size=0.1, random_state=42)
 
-    else:
-        train_path = args.root_train
+    train_dataset = ClassDataset(train_path, train_data, input_types, data_transforms['train'])
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
+                                              shuffle=True, num_workers=args.workers, drop_last=args.drop_last)
 
-        input_types = ['Point', 'None', 'Bad', 'Good', 'Stop']
-        train_img_list = get_filepaths(train_path)
+    validation_dataset = ClassDataset(train_path, val_data, input_types, data_transforms['validation'])
+    validationloader = torch.utils.data.DataLoader(validation_dataset, batch_size=args.batch_size,
+                                                   shuffle=True, num_workers=args.workers, drop_last=args.drop_last)
 
-        # validation_path = args.root_val
-        # validation_img_list = ['Point', 'None', 'Bad', 'Good', 'Stop']
+    test_dataset = ClassDataset(train_path, test_data, input_types, data_transforms['test'])
+    testloader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size,
+                                                   shuffle=True, num_workers=args.workers, drop_last=args.drop_last)
 
-        train_dataset = ClassDataset(train_path, train_img_list, input_types, data_transforms['train'])
-        jitter_dataset = ClassDataset(train_path, train_img_list, input_types, data_transforms['jitter_aug'])
-        flip_dataset = ClassDataset(train_path, train_img_list, input_types, data_transforms['flip'])
-        blur_dataset = ClassDataset(train_path, train_img_list, input_types, data_transforms['blur'])
-
-        full_data_train = torch.utils.data.ConcatDataset([train_dataset, flip_dataset, jitter_dataset, blur_dataset])
-
-        train_size = int(len(full_data_train) * 0.8)
-        val_size = len(full_data_train) - train_size
-        train_dataset, val_dataset = torch.utils.data.random_split(full_data_train, [train_size, val_size])
-
-        trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
-                                                  shuffle=True, num_workers=args.workers, drop_last=args.drop_last)
-
-        # validation_dataset = ClassDataset(validation_path, validation_img_list, data_transforms['validation'])
-        validationloader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size,
-                                                       shuffle=True, num_workers=args.workers, drop_last=args.drop_last)
-        return trainloader, validationloader
+    return trainloader, validationloader, testloader
 
 
 if __name__ == '__main__':
