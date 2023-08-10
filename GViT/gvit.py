b04810a2b1238432638a42f2c15ff832c68a82ec
Eden <edennissinman@gmail.com>
2023-08-10 11:06:13 +0300

    10_08

--- GViT/gvit.py
+++ GViT/gvit.py
@@ -12,21 +12,21 @@ from vit_pytorch import ViT
 warnings.filterwarnings("ignore")
 
 
-def get_index(i, j):
+def get_index(i, j, image_width):
     return j * image_width + i
 
 
-def get_idx(image_width):
+def get_idx(image_width, image_height):
     edge_index = []
     for i in range(image_width):
         for j in range(image_height):
-            current_index = get_index(i, j)
+            current_index = get_index(i, j, image_width)
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     if dx == 0 and dy == 0:
                         continue
                     if 0 <= i + dx < image_width and 0 <= j + dy < image_height:
-                        edge_index.append([current_index, get_index(i + dx, j + dy)])
+                        edge_index.append([current_index, get_index(i + dx, j + dy, image_width)])
 
     edge_index = torch.tensor(edge_index, dtype=torch.long).t()
     return edge_index
@@ -39,7 +39,6 @@ class GCN(nn.Module):
         self.conv2 = GCNConv(hidden_dim, output_dim)
 
     def forward(self, x, edge_index):
-        print(x.shape)
         x = x.view(-1, 3)
         x = self.conv1(x, edge_index)
         x = F.relu(x)
@@ -60,46 +59,44 @@ class GViT(nn.Module):
             depth=6,
             heads=16,
             mlp_dim=2048,
-            channels=1,
+            channels=3,
             dropout=0.1,
             emb_dropout=0.1)
         self.softmax = nn.Softmax()
 
     def forward(self, x, edge_index):
         graphs = self.gnn(x, edge_index)
-        width, height = 224, 224
-        reshaped_graphs = graphs.view(1, 1, height, width)
+        reshaped_graphs = graphs.view(x.size())
         output = self.transformer(reshaped_graphs)
         output = self.softmax(output)
-        print(output)
         return output
 
 
-if __name__ == '__main__':
-    path = '/home/roblab20/PycharmProjects/LongRange/4.png'
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    img = Image.open(path)
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    x = transform(img)
-    # model = GCN(3, 32, 1)
-    # model.to(device)
-
-    model2 = GViT(3, 32, 1, 6)
-    model2.to(device)
-
-    image_width = 224
-    image_height = 224
-
-    edge_index = get_idx(image_width)
-
-    # print('ok')
-    model2.eval()
-    with torch.no_grad():
-        result = model2(x.to(device), edge_index.to(device))
+# if __name__ == '__main__':
+#     path = '/home/roblab20/PycharmProjects/LongRange/4.png'
+#     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#     img = Image.open(path)
+#     transform = transforms.Compose([
+#         transforms.Resize((224, 224)),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ])
+#     x = transform(img)
+#     # model = GCN(3, 32, 1)
+#     # model.to(device)
+#
+#     model2 = GViT(3, 32, 1, 6)
+#     model2.to(device)
+#
+#     image_width = 224
+#     image_height = 224
+#
+#     edge_index = get_idx(image_width)
+#
+#     # print('ok')
+#     model2.eval()
+#     with torch.no_grad():
+#         result = model2(x.to(device), edge_index.to(device))
 
     # print('\n\nResults shape:  ', result.shape)
 
